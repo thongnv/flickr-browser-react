13d256b5056dcad0d164bbb4b226f847a6f408b6
Thong Nguyen <thongola@hotmail.com>
2019-06-02 01:43:53 +0700

    Seach function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 import './App.css';
 import FlickrCard from './card/FlickrCard.js';
 
-class App extends Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             items: []
         };
-        const url = 'https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true';
-        axios.get(url).then(response => {
-            this.setState({items: response.data.items});
-        });
+        this.getData();
     }
 
     render() {
         return (
             <div>
                 <div className="header">
-                    <input className="search-box" type="text" placeholder="Search..."/>
+                    <input className="search-box" type="text" placeholder="Search..."
+                           onKeyUp={this.searchKeyUpHandler.bind(this)} ref="searchInput"/>
                 </div>
                 <div className="search-results">
                     <ul>
                         {this.state.items.map((item, index) => <li key={index}><FlickrCard item={item}/></li>)}
                     </ul>
+                    {!this.state.items.length &&
+                    <div className="no-results">
+                        No results
+                    </div>
+                    }
                 </div>
             </div>
         )
+    }
+
+    searchKeyUpHandler(refName) {
+        this.getData(refName.target.value);
+    }
+
+    getData(input = '') {
+        const url = 'https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=true';
+        axios.get(`${url}&tags=${input}`).then(response => {
+            this.setState({items: response.data.items});
+        });
     }
 }
 
--- src/card/FlickrCard.js
+++ src/card/FlickrCard.js
@@ -4,7 +4,7 @@ import './FlickrCard.css'
 function FlickrCard(props) {
     const item = props.item;
     return <div className="card-item">
-        <a className="thumbnail" href="#" target="_blank"><img src={item.media.m} alt=""/></a>
+        <a className="thumbnail" href={item.link} rel="noopener noreferrer" target="_blank"><img src={item.media.m} alt=""/></a>
         <ul className=" description">
             <li>
                 <span className="label">Title: </span>{item.title}
