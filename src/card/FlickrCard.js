802e160c2e9d27eb2b2cd48a1ad79750476a7fbd
Thong Nguyen <thongola@hotmail.com>
2019-06-06 23:00:18 +0700

    Modified UI

--- src/card/FlickrCard.js
+++ src/card/FlickrCard.js
@@ -10,13 +10,18 @@ function FlickrCard(props) {
                 <span className="label">Title: </span>{item.title}
             </li>
             <li>
-                <span className="label">Author: </span>{item.author}
+                <span className="label">Author: </span>{extractAuthor(item.author)}
             </li>
             <li>
                 <span className="label">Tags: </span>{item.tags}
             </li>
         </ul>
     </div>;
+}
+
+function extractAuthor(value = '') {
+    const authorRegex = '(?<=").+(?=")';
+    return value.match(authorRegex) ? value.match(authorRegex)[0] : '';
 }
 
 export default FlickrCard;
